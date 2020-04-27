02135ebf6810cd7b778c9c2e0daec9e529a33e94
asifRaeesIqbal <asif.iqbal898@hotmail.com>
2020-04-27 23:51:20 +0100

    First commit

---
+++ src/test/java/com/sdjpa/play/SpringDataJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdjpa.play;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
